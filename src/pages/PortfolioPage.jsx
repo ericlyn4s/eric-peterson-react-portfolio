6fb9cab57e5a8b3c0a677d7e252d72b1a66317b9
Eric Peterson <ericmpeterson04@gmail.com>
2024-04-04 20:57:34 -0600

    New changes to portfolio

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,17 +1,32 @@
 function PortfolioPage() {
     return (
     <>
-      <div id="portfolio-grid">
-        <div id="portfolio-columns">
-          <div className="portfolio-box"></div>
-          <div className="portfolio-box"></div>
-          <div className="portfolio-box"></div>
-        </div>
-        <div id="portfolio-columns">
-          <div className="portfolio-box"></div>
-          <div className="portfolio-box"></div>
-          <div className="portfolio-box"></div>
-        </div>
+      <div id="portfolio-body">
+
+        <a id="tech-blog" className="projects" href="https://aqueous-springs-68236-b5e4d5df42c9.herokuapp.com/">
+            <h3>Tech Blog</h3>
+        </a>
+
+        <a id="tale-spin" className="projects" href="https://afternoon-taiga-74070-303988fd2de4.herokuapp.com/">
+            <h3>Tale Spin</h3>   
+        </a>
+
+        <a id="weather-forecaster" className="projects" href="https://ericlyn4s.github.io/weather-forecaster/">
+            <h3>Weather Forecaster</h3>
+        </a>
+
+        <a id="workday-scheduler" className="projects" href="https://ericlyn4s.github.io/daily-workday-calendar/">
+            <h3>Workday Scheduler</h3>   
+        </a>
+
+        <a id="coding-quiz" className="projects" href="https://ericlyn4s.github.io/totally-rad-coding-quiz/" >
+            <h3>Coding Quiz</h3>
+        </a>    
+
+        <a id="password-generator" className="projects" href="https://ericlyn4s.github.io/js-password-generator/">
+            <h3>Password Generator</h3>
+        </a>
+        
       </div>
     </>
     )
